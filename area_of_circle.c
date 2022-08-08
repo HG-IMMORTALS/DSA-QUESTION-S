0d8f8074d96ee58e48dd2dc731d8da9331758e9e
Hackerhemant <95062216+Hackerhemant@users.noreply.github.com>
2022-08-08 16:27:48 +0530

    Add files via upload

---
+++ area_of_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float radius,area;
+printf("Radius of Circle\n");
+scanf("%f",&radius);
+area=3.141*radius*radius;
+printf("Area of Circle will be %f\n",area);
+return 0;
+ } 
